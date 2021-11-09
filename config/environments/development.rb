7771e7135344ee8a94268cf9fdce86e613c64e84
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-11-09 09:03:34 -0300

    Starting to add some styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  ## Configure SCSS usage
+  config.sass.preferred_syntax = :scss
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
